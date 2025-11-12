c1099b28e2afc206547ccbaa7c5a69fa659b8e5e
Jules MESNE <jules.mesne@edu.esiee.fr>
2025-11-12 23:20:05 +0000

    Mon exercice est terminé

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
+"""
+Module main de l'exercice 04-primes.
+Vérifie si un nombre est premier.
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    Vérifie si un entier est un nombre premier ou pas.
+    Retourne True si p est premier, False sinon.
+    """
 
     # votre code ici
 
-    pass
+    if (p % 1 != 0 or p <= 1):
+        return False
+    imax  = int(sqrt(p)) + 1
+    for i in range(2, imax) :
+        if p % i == 0:
+            return False
+    return True
+
 
 #### Fonction principale
 
 
 def main():
+    """
+    Teste et fait quelques appels à la fonction secondaire
+    permettant de vérifier son bon fonctionnement.
+    """
 
     # vos appels à la fonction secondaire ici
 
